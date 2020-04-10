5d519510cb6adde38a5e5e9dc7d9744761b4cd2a
josepablito16 <josepablocif16@gmail.com>
2020-04-09 21:45:55 -0600

    Se agrega metodo de Test y confusion matrix

--- test.py
+++ test.py
@@ -2,65 +2,88 @@ import numpy as np
 import mnist_reader
 from fns_redes_neuronales import *
 import pickle
+import pandas as pd
 
-#Constantes
-'''
-Factor por el que se divide para
-que no baje tan rapido la sigmoide
-'''
-NORMALIZAR=10000.0
+def ConfusionMatrix(real,prediccion):
+	actual=[
+		int(i)
+		for i in real
+		]
 
-#Cantidad de neuronas en la
-#Capa oculta
-NEURONAS_OCULTAS=130
-
-#Neuronas en la capa salida
-NEURONAS_SALIDA=10
-
-
-#Lectura de datos
-print("Lectura de datos...")
-X_test, y_test = mnist_reader.load_mnist ( './data/fashion' , kind = 't10k' )
-
-
-#TEST
-
-modeloCarga=open('modeloFinal','rb')
-flat_thetas=pickle.load(modeloCarga)
-modeloCarga.close()
-
-print("flat_thetas")
-print(flat_thetas)
-
-
-X=X_test/NORMALIZAR
-
-m,n=X.shape
-
-#Y son las labels de cada imagen
-y=y_test.reshape(m,1)
-
-Y=(y==np.array(range(10))).astype(int)
-
-
-#Se construye el modelo
-theta_shapes=np.array([
-	[NEURONAS_OCULTAS,n+1],
-	[NEURONAS_SALIDA,NEURONAS_OCULTAS+1]
-	])
+	prediccionVect=[
+			int(i)
+			for i in prediccion
+			]
 
 
 
-resultado=feed_forward(
-        inflate_matrixes(flat_thetas, theta_shapes),
-        X
-    )
+	y_actu = pd.Series(actual, name='Actual')
+	y_pred = pd.Series(prediccionVect, name='Predicted')
+	df_confusion = pd.crosstab(y_actu, y_pred)
+	print(df_confusion)
 
-prediccion=np.argmax(resultado[-1], axis=1).reshape(m,1)
+def Test(factNormalizar,neuronasOcultas,nombreModelo):
+	#Constantes
+	'''
+	Factor por el que se divide para
+	que no baje tan rapido la sigmoide
+	'''
+	NORMALIZAR=factNormalizar
 
-correctos=((prediccion==y)*1).sum()
-incorrectos=len(y)-correctos
+	#Cantidad de neuronas en la
+	#Capa oculta
+	NEURONAS_OCULTAS=neuronasOcultas
 
-print("Correctas "+str(correctos))
-print("Incorrectos "+str(incorrectos))
-print("Exactitud "+str(correctos*100/float(len(y))))
+	#Neuronas en la capa salida
+	NEURONAS_SALIDA=10
+
+
+	#Lectura de datos
+	print("Lectura de datos...")
+	X_test, y_test = mnist_reader.load_mnist ( './data/fashion' , kind = 't10k' )
+
+
+	#TEST
+
+	modeloCarga=open(nombreModelo,'rb')
+	flat_thetas=pickle.load(modeloCarga)
+	modeloCarga.close()
+
+
+	X=X_test/NORMALIZAR
+
+	m,n=X.shape
+
+	#Y son las labels de cada imagen
+	y=y_test.reshape(m,1)
+
+	Y=(y==np.array(range(10))).astype(int)
+
+
+	#Se construye el modelo
+	theta_shapes=np.array([
+		[NEURONAS_OCULTAS,n+1],
+		[NEURONAS_SALIDA,NEURONAS_OCULTAS+1]
+		])
+
+
+
+	resultado=feed_forward(
+	        inflate_matrixes(flat_thetas, theta_shapes),
+	        X
+	    )
+
+	prediccion=np.argmax(resultado[-1], axis=1).reshape(m,1)
+
+
+	correctos=((prediccion==y)*1).sum()
+	incorrectos=len(y)-correctos
+
+	print("Correctas "+str(correctos))
+	print("Incorrectos "+str(incorrectos))
+	print("Exactitud "+str(correctos*100/float(len(y))))
+
+	return y,prediccion
+
+y,prediccion=Test(1000,533,'modeloFinal2')
+ConfusionMatrix(y,prediccion)
